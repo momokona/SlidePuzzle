01dd39662e6b09afbcedeb6ef4104cd6f7b92d62
momokona <momona.tsukui1216@gmail.com>
2026-01-12 13:25:08 +0900

    Initial commit: Unity Slide Puzzle project structure

---
+++ Assets/SlidePuzzle/Scripts/PuzzlePiece.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+// パズルピース一つのクラス
+// 自分の見た目を表示し、セルIDを保持する
+public class PuzzlePiece : MonoBehaviour
+{
+    // スプライトを描画するためのコンポーネント
+    [SerializeField] SpriteRenderer sprite_renderer_;    // SerializeFieldをつけておくことでUnityがロード時にsprite_rendrer_ にポインタを入れてくれる
+    int cell_id_;   // セルID
+
+    public int GetCellId()
+    {
+        return cell_id_;
+    }
+
+    // 外部から呼ばれてスプライトとセルIDをセット(スプライトはセルごとに異なるので、セットしてもらう必要がある)
+    public void Initialize(int cell_id, Sprite sprite)
+    {
+        cell_id_ = cell_id;
+        sprite_renderer_.sprite = sprite;
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // 必要に応じて _spriteRenderer を使用する処理をここに追加
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
